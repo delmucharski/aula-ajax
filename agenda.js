081a9d36e6cd7261868aec37b7b9a1084d32c58f
DelMucharski <delalucard@gmail.com>
2018-04-11 21:50:01 -0300

    Terminar agenda e testar casos

--- agenda.js
+++ agenda.js
@@ -1,14 +1,49 @@
+
+var n = document.getElementById("nome");
+var t = document.getElementById("telefone");
+var e = document.getElementById("email");
+var lista = document.getElementById("lista");
+
 function gravar(){
-    var n = document.getElementById("nome");
-    var t = document.getElementById("telefone");
-    var e = document.getElementById("email");
-    var lista = document.getElementById("lista");
+
+
+    var divmsg = document.getElementById("msg-erro");
+    divmsg.style.display = "none";
+
+    if (n.value=="" || n.value.length< 3){
+       exibeErro("O campo NOME é obrigatório. (Min: 3 caracteres)");
+       n.focus();
+       return; //early return
+    }
+
+    var regex = /^\([0-9]+\)[0-9]{4}\-[0-9]{4}$/gm;
+
+    if (regex.exec(t.value) == null ){
+        exibeErro("O campo TELEFONE é obrigatório. Exemplo: (xx)xxxx-xxxx");
+        t.focus();
+        return; //early return
+    }
+
+     if (e.value=="" || e.value.search("@")){
+        exibeErro("O campo EMAIL é obrigatório. Exemplo:(abc@gmail.com)");
+        e.focus();
+        return; //early return
+    }
+
+    //MODO ALTERNATIVO DE LÓGICA
+    // if (n.value=="" || t.value=="" || e.value==""){
+    //     alert("Não deixar campo em branco");
+    //     //Reseta o campo de preenchimento
+    //     n.value="";
+    //     t.value="";
+    //     e.value="";
+    // }else{
 
     //Insere novo obj no cadastro
     var novo = '<tr>'
              + '<td>'+n.value+'</td>'
-             + '<td>'+telefone.value+'</td>'
-             + '<td>'+email.value+'</td>'
+             + '<td>'+t.value+'</td>'
+             + '<td>'+e.value+'</td>'
              + '</tr>';
     lista.innerHTML += novo;
     
@@ -18,6 +53,12 @@ function gravar(){
     e.value="";
 }
 
+function exibeErro(msg){
+    var divmsg = document.getElementById("msg-erro");
+    divmsg.innerHTML=msg;
+    divmsg.style.display = "block";
+}
+
 function pegar(){
 
     var dados= [{
@@ -25,6 +66,11 @@ function pegar(){
         nome:"n.value",
         tel:"t.value",
         email:"e.value"
+    },
+    {
+        nome:"a",
+        tel:"b",
+        email:"c"
     }];
 
     var lista = document.getElementById("lista");
