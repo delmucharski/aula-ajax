e76555825773ebdd6bdc92afb865066e330d2830
DelMucharski <delalucard@gmail.com>
2018-04-18 21:46:21 -0300

    Add plugins e bibliotecas de preenchimento de formulários do JQ

--- clientes.js
+++ clientes.js
@@ -26,4 +26,45 @@ $(function(){
             $("tbody").append(tr);
         });
     });
+
+    //ADICIONA NO CADASTRO
+    $("fieldset button").click(function(){
+
+        var n = $("input[name=nome]").val();
+        var e = $("input[name=email]").val();
+        var c = $("input[name=cep]").val();
+        var d = $("input[name=data-nasc]").val();
+
+        var tr = '<tr>'
+                +'<td>'+ n +'</td>'
+                +'<td>'+ e +'</td>'
+                +'<td>'+ c +'</td>'
+                +'<td>'+ d +'</td>'
+                +'</tr>';
+        var tr2 = $(tr).hide().fadeIn(1500);
+        $("tbody").append(tr2);
+    });
+
+    //CEP DIGITAR SOMENTE NUMEROS
+    $("input[name=cep]").keydown(function(ev){
+        if ((ev.keyCode >= 48 && ev.keyCode <= 57) || ev.keyCode == 8 || ev.keyCode == 9){
+            return true;        
+        }else{
+            return false;
+        }
+    });
+
+    //FUNÇÃO DE PLUGIN DE JQUERY PRA DATA E FORMATO
+    $("input[name=data-nasc]").datepicker({dateFormat: "dd/mm/yy"});
+
+    //FUNÇÃO DE VALIDAÇÃO DE EMAIL
+    $("input[name=email]").blur(function(){
+        var valor = $(this).val();
+
+        var regex = /^[a-zA-Z0-9\.\-\_]*@[a-z0-9]*\.[a-z\.]*$/gm;
+        
+        if (regex.test(valor)==false){
+            alert("Email inválido");
+        }
+    });
 });
